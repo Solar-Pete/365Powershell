0a2554a9ce5a6d62be44f66b163ffb153e717ac6
Solar-Pete <Peter.Bunn@solarsystems.co.uk>
2023-07-21 11:51:12 +0100

    Added comments to countdomainswithlicence.ps1
    Added common SKU's and changed some formatting.

--- CountDomainsWithLicence.ps1
+++ CountDomainsWithLicence.ps1
@@ -9,8 +9,18 @@ $users = Get-MsolUser -All
 
 # Loop through the users
 foreach ($user in $users) {
-    # Check if the user has an Exchange Online Plan 2 license
-    if ($user | Where-Object {($_.licenses).AccountSkuId -match "SPB"}) {
+    
+	# Check if the user has an Microsoft 365 Business Premium licence. Change the ENTERSKUHERE to the SKU you are wishing to look for.
+	
+	# Common SKUs are
+	# Exchange Plan 2 - EXCHANGEENTERPRISE
+	# Business Basic - O365_BUSINESS_ESSENTIALS
+	# Business Standard - O365_BUSINESS_PREMIUM
+	# Business Premium - SPB
+	# Office 365 E3 - ENTERPRISEPACK
+	# Office 365 E5 - ENTERPRISEPREMIUM
+	
+    if ($user | Where-Object {($_.licenses).AccountSkuId -match "ENTERSKUHERE"}) {
         # Get the domain of the user
         $domain = $user.UserPrincipalName.Split("@")[1]
 
